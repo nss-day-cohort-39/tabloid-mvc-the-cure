4db5328b3c062c1e10d190c37b80c79c052c2897
Crystal Elsey <hello@crystalelsey.com>
2020-06-23 18:30:51 -0500

    added comments index

---
+++ TabloidMVC/Controllers/CommentController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace TabloidMVC.Controllers
+{
+    [Authorize]
+    public class CommentController : Controller
+    {
+        private readonly CommentRepository _commentRepo;
+        private int postId;
+
+        public CommentController(IConfiguration config)
+        {
+            _commentRepo = new CommentRepository(config);
+        }
+
+        public IActionResult Index()
+        {
+            var comments = _commentRepo.GetCommentsByPostId(postId);
+            return View(comments);
+        }
+
+        public IActionResult Create()
+        {
+            var comment = new Comment();
+            return View(comment);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Comment comment)
+        {
+            try
+            {
+                _commentRepo.AddComment(comment);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(comment);
+            }
+        }
+    }
+}
