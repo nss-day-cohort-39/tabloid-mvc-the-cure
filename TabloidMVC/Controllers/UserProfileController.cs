f6864ba98f7857695f678ea337d05a40e99b5f09
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-06-25 09:43:43 -0500

    ticket 23 completed, user profiles

---
+++ TabloidMVC/Controllers/UserProfileController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        // GET: UserProfileController
+
+        private readonly UserProfileRepository _UserProfileRepository;
+
+        public UserProfileController(IConfiguration config)
+        {
+            _UserProfileRepository = new UserProfileRepository(config);
+        }
+        public ActionResult Index()
+        {
+            var userProfiles = _UserProfileRepository.GetAllUsers();
+            return View(userProfiles);
+        }
+
+        // GET: UserProfileController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserProfileController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserProfileController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserProfileController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserProfileController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserProfileController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserProfileController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
