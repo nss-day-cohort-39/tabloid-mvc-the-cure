b08fe5b313d08f505796ca0d534ef6573629b67b
Jon <durrjp1@gmail.com>
2020-06-26 12:04:09 -0500

    subscribe via post details and view subscribed posts in subscribe nav item

--- TabloidMVC/Repositories/PostRepository.cs
+++ TabloidMVC/Repositories/PostRepository.cs
@@ -246,6 +246,47 @@ namespace TabloidMVC.Repositories
                 }
             }
         }
+        public List<Post> GetSubbedPosts (int cuId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                       SELECT p.Id, p.Title, p.Content, 
+                              p.ImageLocation AS HeaderImage,
+                              p.CreateDateTime, p.PublishDateTime, p.IsApproved,
+                              p.CategoryId, p.UserProfileId,
+                              c.[Name] AS CategoryName,
+                              u.FirstName, u.LastName, u.DisplayName, 
+                              u.Email, u.CreateDateTime, u.ImageLocation AS AvatarImage,
+                              u.UserTypeId, 
+                              ut.[Name] AS UserTypeName
+                         FROM Post p
+                              LEFT JOIN Category c ON p.CategoryId = c.id
+                              LEFT JOIN UserProfile u ON p.UserProfileId = u.id
+                              LEFT JOIN UserType ut ON u.UserTypeId = ut.id
+                              LEFT JOIN Subscription s ON u.Id = s.ProviderUserProfileId
+                        WHERE IsApproved = 1 AND PublishDateTime < SYSDATETIME()
+                        AND WasDeleted = 0 AND s.SubscriberUserProfileId = @cuId
+                        ORDER BY p.PublishDateTime DESC";
+                    cmd.Parameters.AddWithValue("@cuId", cuId);
+                    var reader = cmd.ExecuteReader();
+
+                    var posts = new List<Post>();
+
+                    while (reader.Read())
+                    {
+                        posts.Add(NewPostFromReader(reader));
+                    }
+
+                    reader.Close();
+
+                    return posts;
+                }
+            }
+        }
 
         private Post NewPostFromReader(SqlDataReader reader)
         {
